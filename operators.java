e2d03714da1e5ddcb550e9d0ef722c9dc0ab63ed
veervashishth1708 <veervashishth170824@gmail.com>
2026-05-29 18:00:23 +0530

    arithmetic operator

---
+++ operators.java
@@ -0,0 +1,21 @@
+public class operators {
+    public static void main(String[] args){
+
+        // operators
+        // arithmetic
+        int a = 1 ;
+        int b = 2;
+        int sum = a + b;
+        int diff = a - b;
+        int mul = a * b;
+        int div = a/b;
+        // module
+        int modulo = a%b;
+        System.out.println(sum);
+        System.out.println(diff);
+        System.out.println(mul);
+        System.out.println(div);
+        System.out.println(modulo);
+
+    }
+}
