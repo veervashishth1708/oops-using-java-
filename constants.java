b18c44d19cb21def59a0ce39e1ef4c1c77a4b050
veervashishth1708 <veervashishth170824@gmail.com>
2026-05-29 17:49:40 +0530

    Constant

---
+++ constants.java
@@ -0,0 +1,9 @@
+public class constants {
+    public static void main(String[] args) {
+        // constants
+        int age = 19;
+        age = 20;
+        final float pi = 3.14F;
+        // pi = 1.1F;
+    }
+}
