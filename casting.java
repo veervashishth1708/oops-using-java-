71dc8740221c3759b266e7c540b1cd2bf41e900b
veervashishth1708 <veervashishth170824@gmail.com>
2026-05-29 17:41:25 +0530

    Casting

---
+++ casting.java
@@ -0,0 +1,16 @@
+public class casting {
+    public static void main(String[] args) {
+        // casting
+        // 2 types
+        // implicite
+        double price = 100.00;
+        double finalprice = price+18;
+        System.out.println(finalprice);
+        // explicite casting
+        int p = 100;
+        int fp = p + (int)18.18;
+        System.out.println(fp);
+
+
+    }
+}
