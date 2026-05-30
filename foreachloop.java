df5d698ea7813f0e7e3c37c59e4985600fedf61b
veervashishth1708 <veervashishth170824@gmail.com>
2026-05-30 23:09:11 +0530

    for each loop

---
+++ foreachloop.java
@@ -0,0 +1,12 @@
+public class foreachloop {
+        public static void main(String[] args) {
+
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        for(int num : numbers) {
+            System.out.println(num);
+        }
+
+    }
+
+}
