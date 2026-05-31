6484eff85cc18011b8ae9eaebfbe062a25ee5e61
veervashishth1708 <veervashishth170824@gmail.com>
2026-05-31 18:36:46 +0530

    Inheritance2

---
+++ Inheritance2.java
@@ -0,0 +1,23 @@
+class Animal {
+
+    void eat() {
+        System.out.println("Animal is eating");
+    }
+}
+
+class Dog extends Animal {
+
+    void bark() {
+        System.out.println("Dog is barking");
+    }
+}
+
+public class Inheritance2 {
+    public static void main(String[] args) {
+
+        Dog d = new Dog();
+
+        d.eat();
+        d.bark();
+    }
+}
