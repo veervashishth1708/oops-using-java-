7502c7708432dc256cff5505fc383661532ff941
veervashishth1708 <veervashishth170824@gmail.com>
2026-06-01 21:43:37 +0530

    Method Overriding (Run-Time Polymorphism)

---
+++ Overriding2.java
@@ -0,0 +1,22 @@
+class Animal {
+
+    void sound() {
+        System.out.println("Animal makes sound");
+    }
+}
+
+class Dog extends Animal {
+
+    @Override
+    void sound() {
+        System.out.println("Dog barks");
+    }
+}
+
+public class Overriding2 {
+    public static void main(String[] args) {
+
+        Dog d = new Dog();
+        d.sound();
+    }
+}
