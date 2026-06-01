fb722842edd50aac099d96b21db86e2bd2a6bdbf
veervashishth1708 <veervashishth170824@gmail.com>
2026-06-01 21:39:51 +0530

    Method Overriding (Inheritance)

--- Inheritance7.java
+++ Inheritance7.java
@@ -1,3 +1,22 @@
+class Animal {
+
+    void sound() {
+        System.out.println("Animal makes sound");
+    }
+}
+
+class Dog extends Animal {
+
+    @Override
+    void sound() {
+        System.out.println("Dog barks");
+    }
+}
+
 public class Inheritance7 {
-    
+    public static void main(String[] args) {
+
+        Dog d = new Dog();
+        d.sound();
+    }
 }
