fad7ece7ab43c4d81697bdf667a12453e76b7c03
veervashishth1708 <veervashishth170824@gmail.com>
2026-06-01 23:03:46 +0530

    Multiplication table

---
+++ Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+
+        if(a > b)
+            System.out.println(a + " is larger");
+        else
+            System.out.println(b + " is larger");
+
+        sc.close();
+    }
+}
