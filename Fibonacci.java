fb73efd36ff6fa2861a7d48c2543020c90a86963
veervashishth1708 <veervashishth170824@gmail.com>
2026-06-02 23:45:25 +0530

    Fibonacci Series

---
+++ Fibonacci.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter terms: ");
+        int n = sc.nextInt();
+
+        int a = 0, b = 1;
+
+        System.out.print("Series: ");
+
+        for(int i = 1; i <= n; i++) {
+            System.out.print(a + " ");
+
+            int c = a + b;
+            a = b;
+            b = c;
+        }
+
+        sc.close();
+    }
+}
