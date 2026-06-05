deb8efe6b3afc61a1ce889415d9270cd0e2786d9
veervashishth1708 <veervashishth170824@gmail.com>
2026-06-05 23:30:25 +0530

    simple intrest cal.

---
+++ SimpleInterest.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class SimpleInterest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double p, r, t, si;
+
+        System.out.print("Enter Principal: ");
+        p = sc.nextDouble();
+
+        System.out.print("Enter Rate: ");
+        r = sc.nextDouble();
+
+        System.out.print("Enter Time: ");
+        t = sc.nextDouble();
+
+        si = (p * r * t) / 100;
+
+        System.out.println("Simple Interest = " + si);
+    }
+}
