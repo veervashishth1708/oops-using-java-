14017314ce2d2bdf21e4215af220f027ddb91650
veervashishth1708 <veervashishth170824@gmail.com>
2026-06-05 23:29:28 +0530

    largest array

---
+++ LargestArray.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class LargestArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
+
+        for(int i = 0; i < n; i++) {
+            System.out.print("Enter element: ");
+            arr[i] = sc.nextInt();
+        }
+
+        int largest = arr[0];
+
+        for(int i = 1; i < n; i++) {
+            if(arr[i] > largest)
+                largest = arr[i];
+        }
+
+        System.out.println("Largest Element = " + largest);
+    }
+}
