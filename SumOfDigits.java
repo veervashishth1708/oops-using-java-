cfa0e9db5d0f47cfbd4c29fff5f22b5f42e4fade
veervashishth1708 <veervashishth170824@gmail.com>
2026-06-10 22:45:49 +0530

    sum of digits of numbers

--- SumOfDigits.java
+++ SumOfDigits.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class SumOfDigits {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        System.out.println("sum of digits ");
         System.out.print("Enter a number: ");
         int num = sc.nextInt();
 
