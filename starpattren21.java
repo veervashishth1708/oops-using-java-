87b7ea026a1539a9ea070257592170fb0b3a2cba
veervashishth1708 <veervashishth170824@gmail.com>
2026-06-10 22:39:41 +0530

    update

--- starpattren21.java
+++ starpattren21.java
@@ -25,5 +25,6 @@ public class starpattren21 {
 
             System.out.println();
         }
+        System.out.println("****");
     }
 }
--- starpattren22.java
+++ starpattren22.java
@@ -15,6 +15,6 @@ public class starpattren22 {
 
         System.out.println("    *");
         System.out.println("    *");
-        System.out.println("     *");
+        System.out.println("    *");
     }
 }
