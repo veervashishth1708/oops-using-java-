171ffb781a53c352156d2527b4db9f5ba354dde9
veervashishth1708 <veervashishth170824@gmail.com>
2026-06-19 23:42:40 +0530

    dowhile

---
+++ dowhile2.java
@@ -0,0 +1,12 @@
+public class dowhile2 {
+        public static void main(String[] args) {
+        System.out.println("while loop ");
+        int i = 1;
+
+        do {
+            System.out.println("Iteration: " + i);
+            i++;
+        } while(i <= 5);
+
+    }
+}
