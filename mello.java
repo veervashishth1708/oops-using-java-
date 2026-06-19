24e71dea92452fc2ec9522df41d4a4ae70493d1f
veervashishth1708 <veervashishth170824@gmail.com>
2026-06-19 23:40:59 +0530

    update

---
+++ mello.java
@@ -0,0 +1,16 @@
+class Main {
+    public static void mello(String[] args) {
+
+        System.out.print("Hello World from java ");
+        //variable
+        String name = "veer";
+        int age  = 19;
+        String neighboor = "akku";
+        int age2 = 45;
+
+        String  friend = neighboor;
+
+
+        // tyoes in java 
+    }
+}
